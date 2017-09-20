fce13fa4193e96b1c71c4a9f970e5c105cee046e
beergeek <brett@puppet.com>
2017-09-20 15:24:57 +1000

    Smarter way of disabling to prevent issues

--- tasks/init.rb
+++ tasks/init.rb
@@ -3,8 +3,13 @@ require 'open3'
 require 'puppet'
 
 begin
-  cmd_string = "puppet config set noop false --section agent"
+  Puppet.initialize_settings
+  @configfile = Puppet::settings['config']
+  x = ::Puppet::Resource.new('ini_setting', 'noop', :parameters => {'ensure' => 'absent','path' => @configfile,'section' => 'agent','setting' => 'noop', 'value' => 'false'})
+  result, report = ::Puppet::Resource.indirection.save(x)
+  cmd_string = "puppet config print noop --section agent"
   _stdout, stderr, status = Open3.capture3(cmd_string)
-  raise Puppet::Error, stderr if status != 0
-  { status: status }
+  puts "Noop is now #{_stdout}"
+rescue => e
+  raise "Could not manage noop: #{e.to_s}"
 end
