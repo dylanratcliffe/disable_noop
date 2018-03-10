3cd49b79512a3b007be22c5b9cc616f02f0ee47d
beergeek <brett@puppet.com>
2018-03-11 07:46:23 +1100

    Update code and dependencies

--- tasks/init.rb
+++ tasks/init.rb
@@ -4,12 +4,12 @@ require 'puppet'
 
 begin
   Puppet.initialize_settings
-  @configfile = Puppet::settings['config']
-  x = ::Puppet::Resource.new('ini_setting', 'noop', :parameters => {'ensure' => 'absent','path' => @configfile,'section' => 'agent','setting' => 'noop', 'value' => 'false'})
-  result, report = ::Puppet::Resource.indirection.save(x)
-  cmd_string = "puppet config print noop --section agent"
-  _stdout, stderr, status = Open3.capture3(cmd_string)
-  puts "Noop is now #{_stdout}"
+  @configfile = Puppet.settings['config']
+  x = ::Puppet::Resource.new('ini_setting', 'noop', parameters: { ensure: 'absent', path: @configfile, section: 'agent', setting: 'noop' })
+  _result, _report = ::Puppet::Resource.indirection.save(x)
+  cmd_string = 'puppet config print noop --section agent'
+  stdout, _stderr, _status = Open3.capture3(cmd_string)
+  puts "Noop is now #{stdout}"
 rescue => e
-  raise "Could not manage noop: #{e.to_s}"
+  raise "Could not manage noop: #{e}"
 end
