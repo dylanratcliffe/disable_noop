04d779684242818b927bf91fe881b2412dbf5a02
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2018-07-10 16:32:36 +1000

    Removed dependance on shelling out

--- tasks/init.rb
+++ tasks/init.rb
@@ -7,9 +7,8 @@ begin
   @configfile = Puppet.settings['config']
   x = ::Puppet::Resource.new('ini_setting', 'noop', parameters: { ensure: 'absent', path: @configfile, section: 'agent', setting: 'noop' })
   _result, _report = ::Puppet::Resource.indirection.save(x)
-  cmd_string = 'puppet config print noop --section agent'
-  stdout, _stderr, _status = Open3.capture3(cmd_string)
-  puts "Noop is now #{stdout}"
+  Puppet.initialize_settings
+  puts "Noop is now #{Puppet.settings['noop']}"
 rescue => e
   raise "Could not manage noop: #{e}"
 end
